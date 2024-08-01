8513221a6894ac4820344dd3d50a3b0aa44ef6b7
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-08-02 02:38:48 +0530

    log.txt added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import errorHandler from 'errorhandler';
 import route from './routes/userRoute.js';
+import fs from 'fs';
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,19 +15,24 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL).then(function() {
     console.log("DB connection");
 })
+app.use(function(req, res, next){
+    fs.appendFile("log.txt", "\n "+ Date.now() + " : " + req.method + " " + req.path,(err) => next(err));
+})
+app.use('/api/users', route);
+
 app.all("*", (req, res, next) => {
     const err = new Error(`Can't find ${req.originalUrl} on this server`);
     err.statusCode = 404;
     err.message = "Page not found";
     next(err);
 })
-app.use('/api/users', route);
 //global error handling middleware
+
 app.use((error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.message = error.message || "Internal server error";
-    res.status(error.statusCode).json({statusCode: error.statusCode, message: error.message});
-})
+    return res.status(error.statusCode).json({statusCode: error.statusCode, message: error.message});
+});
 // app.use(errorHandler());
 app.listen(PORT, function(){
     console.log("Server listening on port "+ PORT);
