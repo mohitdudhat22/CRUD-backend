79370c296d40926a194bef98b6dda7527e2f785e
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-08-02 02:23:58 +0530

    global error handler

--- index.js
+++ index.js
@@ -14,8 +14,20 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL).then(function() {
     console.log("DB connection");
 })
+app.all("*", (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.statusCode = 404;
+    err.message = "Page not found";
+    next(err);
+})
 app.use('/api/users', route);
-app.use(errorHandler());
+//global error handling middleware
+app.use((error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.message = error.message || "Internal server error";
+    res.status(error.statusCode).json({statusCode: error.statusCode, message: error.message});
+})
+// app.use(errorHandler());
 app.listen(PORT, function(){
     console.log("Server listening on port "+ PORT);
 });
